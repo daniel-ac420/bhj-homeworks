acd33ecd00aa5dd749ee7150a60f9439273de260
Daniel <popov-ac420@yandex.com>
2024-02-06 17:45:24 +0500

    v1.1

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,10 +1,10 @@
 let timerElement = document.querySelector("#timer");
-let timerNumber = Number(timer.textContent) - 1;
+let timerNumber = Number(timerElement.textContent) - 1;
 
 let timerID = setInterval(function() {
 	timerElement.innerHTML = timerNumber--;
 	
-	if (timer.innerHTML === "0") {
+	if (timerElement.innerHTML === "0") {
 		clearInterval(timerID);
 		alert("Вы победили в конкурсе!");
 	}
