94773dd3165c5ddb2a18f8cee3315ebc6b6d2557
Daniel <popov-ac420@yandex.com>
2024-02-08 21:49:25 +0500

    v1.1

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,16 +1,42 @@
-let button = document.querySelector(".dropdown__value");
-let list = document.querySelector(".dropdown__list");
-let listItems = document.querySelectorAll(".dropdown__link");
+/* --- #1 ВАРИАНТ ДЛЯ ОДНОЙ КНОПКИ --- */
+//let button = document.querySelector(".dropdown__value");
+//let list = document.querySelector(".dropdown__list");
+//let listItems = document.querySelectorAll(".dropdown__link");
+//
+//button.addEventListener("click", function() {
+//	list.classList.toggle("dropdown__list_active");
+//})
+//
+//listItems.forEach(function(listItem) {
+//	listItem.addEventListener("click", function changeValue(event) {
+////		console.log(this.textContent.trim());
+//		button.innerHTML = this.textContent.trim();
+//		list.classList.remove("dropdown__list_active");
+//		event.preventDefault();
+//	})
+//})
 
-button.addEventListener("click", function() {
-	list.classList.toggle("dropdown__list_active");
-})
 
-listItems.forEach(function(listItem) {
-	listItem.addEventListener("click", function changeValue(event) {
-//		console.log(this.textContent.trim());
-		button.innerHTML = this.textContent.trim();
-		list.classList.remove("dropdown__list_active");
-		event.preventDefault();
+
+/* --- #2 ВАРИАНТ ДЛЯ НЕСКОЛЬКИХ КНОПОК --- */
+let buttons = document.querySelectorAll(".dropdown__value");
+
+for (let button of buttons) {
+	let list = button.nextElementSibling;
+//	console.log(button.nextElementSibling);
+	
+	button.addEventListener("click", function() {
+		list.classList.toggle("dropdown__list_active");
 	})
-})
+
+	let listItems = list.querySelectorAll(".dropdown__link");;
+//	console.log(listItems);
+	
+	listItems.forEach(function(listItem) {
+		listItem.addEventListener("click", function changeValue(event) {
+			button.innerHTML = this.textContent.trim();
+			list.classList.remove("dropdown__list_active");
+			event.preventDefault();
+		})
+	})
+}
