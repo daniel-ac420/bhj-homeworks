3b80f43244128393da10d325d7a4f02a71cbf286
Daniel <popov-ac420@yandex.com>
2024-02-09 19:47:22 +0500

    v1.1

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,21 +1,52 @@
-const tabs = document.querySelectorAll(".tab__navigation .tab");
+// /* --- ДЛЯ ОДНОГО НАБОРА ВКЛАДОК НА СТРАНИЦЕ --- */
+//const tabs = document.querySelectorAll(".tab__navigation .tab");
+//
+//const tabsContent = document.querySelectorAll(".tab__contents .tab__content");
+//
+//tabs.forEach(function(tab, index) {
+//	tab.addEventListener("click", function() {
+//		tabs.forEach(function(tab) {
+//			tab.classList.remove("tab_active");
+//		})
+//		
+//		this.classList.add("tab_active");
+//		
+//		tabsContent.forEach(function(tabContent) {
+//			tabsContent.forEach(function(tabContent) {
+//				tabContent.classList.remove("tab__content_active");
+//			})
+//			
+//			tabsContent[index].classList.add("tab__content_active");
+//		})
+//	})
+//})
 
-const tabsContent = document.querySelectorAll(".tab__contents .tab__content");
 
-tabs.forEach(function(tab, index) {
-	tab.addEventListener("click", function() {
-		tabs.forEach(function(tab) {
-			tab.classList.remove("tab_active");
-		})
-		
-		this.classList.add("tab_active");
-		
-		tabsContent.forEach(function(tabContent) {
-			tabsContent.forEach(function(tabContent) {
-				tabContent.classList.remove("tab__content_active");
+
+/* --- ДЛЯ МНОЖЕСТВА НАБОРОВ ВКЛАДОК НА СТРАНИЦЕ --- */
+
+const allTabs = document.querySelectorAll(".tabs");
+
+for (let tabsElements of allTabs) {
+	const tabs = tabsElements.querySelectorAll(".tab__navigation .tab");
+
+	const tabsContent = tabsElements.querySelectorAll(".tab__contents .tab__content");
+
+	tabs.forEach(function(tab, index) {
+		tab.addEventListener("click", function() {
+			tabs.forEach(function(tab) {
+				tab.classList.remove("tab_active");
 			})
-			
-			tabsContent[index].classList.add("tab__content_active");
+
+			this.classList.add("tab_active");
+
+			tabsContent.forEach(function(tabContent) {
+				tabsContent.forEach(function(tabContent) {
+					tabContent.classList.remove("tab__content_active");
+				})
+
+				tabsContent[index].classList.add("tab__content_active");
+			})
 		})
 	})
-})
+}
