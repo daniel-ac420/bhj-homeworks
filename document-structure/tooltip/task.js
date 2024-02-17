00bc1323832428a23f6fe275e40a6b4f02c9d193
Daniel <popov-ac420@yandex.com>
2024-02-17 18:47:21 +0500

    v1.1

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,29 +2,28 @@ const toolTipsElements = document.querySelectorAll(".has-tooltip");
 
 for (let toolTipsElement of toolTipsElements) {
 	toolTipsElement.addEventListener("click", function(event) {
-		let oldToolTips = document.querySelectorAll(".tooltip");
-
-		for (let oldToolTip of oldToolTips) {
-			oldToolTip.remove();
-		}
-		
-//		console.log(this.title);
 		event.preventDefault();
-		toolTipsElement.style.position = "relative";
-		toolTipsElement.offsetTop = 10;
 		
-		let newToolTip = document.createElement("div");
-		newToolTip.textContent = toolTipsElement.title;
-		newToolTip.classList.add("tooltip");
-		newToolTip.classList.add("tooltip_active");
-		newToolTip.style.position = "absolute";
-		newToolTip.style.left = 0;
-//		newToolTip.style.right = 0;
-		newToolTip.style.top = "50%";
-//		newToolTip.offsetTop = 25;
-//		newToolTip.offsetLeft = 0;
-//		newToolTip.offsetRight = 0;
-		toolTipsElement.appendChild(newToolTip);
-//		console.log(newToolTip);
+		if (this.nextElementSibling.classList.contains("tooltip_active")) {
+			this.nextElementSibling.remove();
+		} else {
+			let oldToolTips = document.querySelectorAll(".tooltip");
+
+			for (let oldToolTip of oldToolTips) {
+				oldToolTip.remove();
+			}
+
+			let newToolTip = document.createElement("div");
+			newToolTip.textContent = toolTipsElement.title;
+			newToolTip.classList.add("tooltip");
+			newToolTip.classList.add("tooltip_active");
+
+			let toolTipsElementRect = this.getBoundingClientRect();
+
+			newToolTip.style.left = `${toolTipsElementRect.left}px`;
+			newToolTip.style.top = `${toolTipsElementRect.bottom}px`;
+			
+			this.insertAdjacentElement("afterEnd", newToolTip);
+		}
 	})
 }
