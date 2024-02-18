6e549b8b25ccdb32bfa0d29d3d019f6b744c5ac1
Daniel <popov-ac420@yandex.com>
2024-02-18 14:58:32 +0500

    v1.2

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -6,29 +6,27 @@ const button = document.querySelector("#tasks__add");
 button.addEventListener("click", task);
 
 function task(event) {
-	if (event.type === "click") {
-		if (input.value.trim() !== "") {
-			event.preventDefault();
-			
-			let newTask = document.createElement("div");
-			newTask.classList.add("task");
-			newTask.insertAdjacentHTML("afterbegin", `
-				<div class="task__title">
-					${input.value}
-				</div>
-				<a href="#" class="task__remove">&times;</a>
-			`);
-			
-			input.value = "";
-			tasksList.append(newTask);
-			
-			const removeTask = newTask.querySelector(".task__remove");
-			removeTask.title = "Удалить задачу";
-			removeTask.addEventListener("click", function() {
-				this.closest(".task").remove();
-			})
-		} else {
-			event.preventDefault();
-		}
+	event.preventDefault();
+	
+	if (input.value.trim() === "") {
+		return;
 	}
+	
+	let newTask = document.createElement("div");
+	newTask.classList.add("task");
+	newTask.insertAdjacentHTML("afterbegin", `
+		<div class="task__title">
+			${input.value}
+		</div>
+		<a href="#" class="task__remove">&times;</a>
+	`);
+
+	input.value = "";
+	tasksList.append(newTask);
+
+	const removeTask = newTask.querySelector(".task__remove");
+	removeTask.title = "Удалить задачу";
+	removeTask.addEventListener("click", function() {
+		this.closest(".task").remove();
+	})
 }
